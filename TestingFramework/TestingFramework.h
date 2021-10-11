8fd53b01e822171bfa3cb6d559fa5b106c110777
mohamad.barek <mohamad.barek@ideatolife.me>
2021-10-11 12:03:26 +0300

    Initial Commit

---
+++ TestingFramework/TestingFramework.h
@@ -0,0 +1,18 @@
+//
+//  TestingFramework.h
+//  TestingFramework
+//
+//  Created by mohamad barek on 10/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestingFramework.
+FOUNDATION_EXPORT double TestingFrameworkVersionNumber;
+
+//! Project version string for TestingFramework.
+FOUNDATION_EXPORT const unsigned char TestingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestingFramework/PublicHeader.h>
+
+
